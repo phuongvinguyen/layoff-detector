724c50a974cf7019dfbc9c44ad1a2cd493b9657f
TimTheHero <117226730+TimTheHero@users.noreply.github.com>
2026-06-12 12:13:46 +0200

    feat: Create ml_test.ipynb
    Cret ml_test.ipynb a ML pipeline
    The script does the following:
    1. Generate negaive samples based on the real input data.
    2. Feature engineering: Boolean flags for all variables 0 -> nan, 1 -> any value
    3. numeric features: fill nan with median.
    4. trains 6 ml models with StratifiedKFold n = 5:
    Random Forest,XGBoost, GradientBoosting, Logistic Regression, SVM and KNN.
    5. Analyses the result

--- config/config.py
+++ config/config.py
@@ -46,3 +46,7 @@ GET_FINANCIAL_DATA_COMBINED = {
 MERGED_DATA = {
     "MERGED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv"
 }
+
+PIPELINE = {
+    "INPUT_CSV_PATH":  DATA_PROCESSED / "merged_bs_cs_fd.csv"
+}
