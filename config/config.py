bc275323b6b6dfc1a295afc4eb38ee2764a43831
vinguyen <phuongvinguyen2000@gmail.com>
2026-06-15 09:13:13 +0200

    feat: labeling layoff

--- config/config.py
+++ config/config.py
@@ -45,5 +45,6 @@ GET_FINANCIAL_DATA_COMBINED = {
 }
 
 MERGED_DATA = {
-    "MERGED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv"
+    "MERGED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv",
+    "LABELED_OUTPUT_CSV_PATH": DATA_PROCESSED / "labeled_merged_data.csv"
 }
