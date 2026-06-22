cffe29bbeca64effa06e6aa68e7404e8c91be2d9
Alfredo Martins <34483729+CodeTyperPro@users.noreply.github.com>
2026-06-22 21:11:10 +0200

    feat: files organized, directories fixed

--- layoff/__init__.py
+++ layoff/__init__.py
@@ -3,4 +3,4 @@
 __version__ = "0.1"
 __author__ = "VI GROUP"
 
-from . import config, data, custom_funcs, test_config, test_custom_funcs
+from . import config, custom_funcs, test_config, test_custom_funcs
--- layoff/config.py
+++ layoff/config.py
@@ -21,6 +21,9 @@ DATA_DIR       = BASE_DIR / "data"
 DATA_RAW       = DATA_DIR / "raw"
 DATA_CLEANED   = DATA_DIR / "cleaned"
 DATA_PROCESSED = DATA_DIR / "processed"
+DATA_BALANCE_SHEET = DATA_DIR / "balance_sheet"
+DATA_CASHFLOWS = DATA_DIR / "cashflows"
+DATA_FINANCIALS = DATA_DIR / "financials"
 
 SRC_DIR = BASE_DIR / "src"
 
@@ -36,7 +39,7 @@ GET_RAW = {
 }
 
 GET_FINANCIAL_DATA = {
-    "INPUT_CSV_PATH":  "../src/cleaned_layoffs.csv",
+    "INPUT_CSV_PATH":  DATA_CLEANED / "cleaned_layoffs.csv",
     "fyi_layoffs":  DATA_RAW / "fyi_layoffs.csv",
     "FILTERED_CSV_PATH": DATA_PROCESSED / "filtered_layoffs.csv",
     "LAYOFFS_WITH_TICKERS_CSV_PATH": DATA_PROCESSED / "layoffs_with_tickers.csv",
@@ -46,18 +49,23 @@ GET_FINANCIAL_DATA = {
 }
 
 GET_FINANCIAL_DATA_COMBINED = {
-    "INPUT_CSV_PATH": "../src/final_layoffs.csv",
+    "INPUT_CSV_PATH": DATA_PROCESSED / "final_layoffs.csv",
     "LAYOFF_FILTERED_COMBINED_CSV_PATH": DATA_PROCESSED / "layoffSizeFiltered_combined.csv",
     "LAYOFFS_WITH_TICKERS_COMBINED_CSV_PATH": DATA_PROCESSED / "layoffs_with_tickers_combined.csv",
-    "CLEANED_LAYOFF_COMBINED_CSV_PATH": DATA_CLEANED/"cleaned_layoffs_with_tickers_combined.csv"
+    "CLEANED_LAYOFF_COMBINED_CSV_PATH": DATA_CLEANED / "cleaned_layoffs_with_tickers_combined.csv"
 }
 
 MERGED_DATA = {
-    "MERGED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv"
+    "MERGED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv",
+    "LABELED_OUTPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv"
 }
 
 PIPELINE = {
-    "INPUT_CSV_PATH":  DATA_PROCESSED / "merged_bs_cs_fd.csv"
+    "INPUT_CSV_PATH": DATA_PROCESSED / "merged_bs_cs_fd.csv",
 }
+
+LABELED = {
+    "LABELED_OUTPUT_CSV_PATH": DATA_PROCESSED / "labeled_merged_data.csv",
+} 
 
 # Configuration variables
